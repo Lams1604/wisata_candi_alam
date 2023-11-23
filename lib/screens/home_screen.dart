75384e8007847437b9b14bb6dedc24eb8b137056
VenEsuella <126873233+VenEsuella@users.noreply.github.com>
2023-11-23 10:58:33 +0700

    update_wisata_candi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
  import 'package:flutter/Material.dart';
 import 'package:flutter_alam/data/candi_data.dart';
+import '../Widget/item_card.dart';
 import '../models/candi.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,24 +10,24 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // TODO: 1. Buat appbar dengan judul wisata candi
-      appBar: AppBar(title: Text('Wisata Candi'),),
-      // TODO: 2. Buat body dengan GridView.builder
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2
-          ),
-          padding: EdgeInsets.all(8.0),
-          itemCount: candiList.length,
-          itemBuilder: (contex,index){
-            Candi candi = candiList[index];
-            return ItemCard(candi: candi);
-          },),
-      // TODO: 3. Buat ItemCard sebagai return value dari GridView.builder
-    );
-  }
-}
+ class _HomeScreenState extends State<HomeScreen> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+// TODO: 1. Buat appbar dengan judul Wisata Candi
+         appBar: AppBar(title: Text('Wisata Candi')),
+// TODO: 2. Buat body dengan GridView.builder
+         body: GridView.builder(
+           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+               crossAxisCount: 2
+           ),
+           padding: EdgeInsets.all(8.0),
+           itemCount: candiList.length,
+           itemBuilder: (context,index) {
+             Candi candi = candiList[index];
+             return ItemCard(candi: candi);
+           },)
+// TODO: 3. Buat ItemCard sebagai return value dari GridView.Builder
+     );
+   }
+ }
